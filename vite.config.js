e11ad56118848fbb3556008dc2aceaf84f908c4d
kurmapusoudamini-dev <kurmapusoudamini@gmail.com>
2025-08-01 16:23:50 +0000

    Try fixing blank page

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ["yss-fe.chataid.com"]
-  }
+   plugins: [react()],
+  base: '/gday/',
 });
